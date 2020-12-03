a55324ca98d98ec9a3c06e81238e1e2c8085a4ef
Shelley Vohr <shelley.vohr@gmail.com>
2020-12-02 18:59:37 -0800

    fix: screen EventEmitter methods with remote

--- lib/browser/api/screen.ts
+++ lib/browser/api/screen.ts
@@ -1,6 +1,13 @@
+import { EventEmitter } from 'events';
+
 const { createScreen } = process._linkedBinding('electron_common_screen');
 
 let _screen: Electron.Screen;
+
+const ensureScreen = () => {
+  _screen = createScreen();
+  EventEmitter.call(_screen);
+};
 
 // We can't call createScreen until after app.on('ready'), but this module
 // exposes an instance created by createScreen. In order to avoid
@@ -8,9 +15,7 @@ let _screen: Electron.Screen;
 // we export a proxy which lazily calls createScreen on first access.
 export default new Proxy({}, {
   get: (target, prop: keyof Electron.Screen) => {
-    if (_screen === undefined) {
-      _screen = createScreen();
-    }
+    if (_screen === undefined) ensureScreen();
     const v = _screen[prop];
     if (typeof v === 'function') {
       return v.bind(_screen);
@@ -18,21 +23,19 @@ export default new Proxy({}, {
     return v;
   },
   ownKeys: () => {
-    if (_screen === undefined) {
-      _screen = createScreen();
-    }
+    if (_screen === undefined) ensureScreen();
     return Reflect.ownKeys(_screen);
   },
+  getPrototypeOf: (target) => {
+    const targetPrototype = Reflect.getPrototypeOf(target);
+    return Object.assign(EventEmitter.prototype, targetPrototype);
+  },
   has: (target, prop: string) => {
-    if (_screen === undefined) {
-      _screen = createScreen();
-    }
+    if (_screen === undefined) ensureScreen();
     return prop in _screen;
   },
   getOwnPropertyDescriptor: (target, prop: string) => {
-    if (_screen === undefined) {
-      _screen = createScreen();
-    }
+    if (_screen === undefined) ensureScreen();
     return Reflect.getOwnPropertyDescriptor(_screen, prop);
   }
 });
